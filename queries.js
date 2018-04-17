909376d69d58174a8d3f9b5d00c55d234c918d84
Ben Casalino <bencasalino@gmail.com>
2018-04-17 11:42:15 -0600

    queries

---
+++ queries.js
@@ -0,0 +1,34 @@
+const database = require("./database-connection");
+
+module.exports = {
+  list(path) {
+    return database(path).select();
+  },
+  //get
+  read(name, path) {
+    return database(path)
+      .select()
+      .where("name", name);
+    // .first();
+  },
+  // post
+  create(body, path) {
+    return database(path)
+      .insert(body)
+      .returning("*")
+      .then(record => record[0]);
+  },
+  // put
+  update(name, body, path) {
+    return database(path)
+      .update(body)
+      .where("name", name)
+      .returning("*")
+      .then(record => record[0]);
+  },
+  delete(name, path) {
+    return database(path)
+      .delete()
+      .where("name", name);
+  }
+};
